f8cb765d8d19ac977c284c9e7330a8896bfa653a
RdetHei <arcadial076@gmail.com>
2025-08-28 08:57:18 +0700

    Add files via upload

---
+++ stuarz/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <title>STUARZ</title>
+</head>
+<body>
+    <?php include 'view/landing/header.php'; ?>
+    <?php include 'view/landing/hero.php'; ?>
+    <?php include 'view/landing/footer.php'; ?>
+</body>
+</html>
