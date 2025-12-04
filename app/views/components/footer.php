e000d5eeef3e3af33b9d1976690d4035b0f4552c
RdetHei <arcadial076@gmail.com>
2025-12-04 07:31:48 +0700

    dam

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -38,19 +38,19 @@
                         </svg>
                     </a>
  
-                    <a href="#" class="text-white hover:text-indigo-400" title="X">
+                    <a href="https://twitter.com/rdet" class="text-white hover:text-indigo-400" title="X">
                         <svg class="w-5 h-5" fill="#fff" viewBox="0 0 24 24">
                             <path d="M17.53 3H21.5l-7.19 7.72L23 21h-7.5l-5.36-6.01L3 21H-.01l7.64-8.2L1 3h7.5l4.66 5.22L17.53 3zm-2.13 16h2.13l-6.13-6.87-2.13 2.29L15.4 19z"/>
                         </svg>
                     </a>
  
-                    <a href="#" class="text-white hover:text-indigo-400" title="GitHub">
+                    <a href="https://github.com/RdetHei/stuarz" class="text-white hover:text-indigo-400" title="GitHub">
                         <svg class="w-5 h-5" fill="#fff" viewBox="0 0 24 24">
                             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.089-.745.083-.729.083-.729 1.205.084 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.775.418-1.304.762-1.604-2.665-.304-5.466-1.334-5.466-5.93 0-1.31.469-2.381 1.236-3.221-.124-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23a11.52 11.52 0 013.003-.404c1.018.005 2.045.138 3.003.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.873.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.803 5.624-5.475 5.921.43.371.823 1.102.823 2.222v3.293c0 .322.218.694.825.576C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
                         </svg>
                     </a>
  
-                    <a href="#" class="text-white hover:text-indigo-400" title="YouTube">
+                    <a href="https://youtube.com/@rdet" class="text-white hover:text-indigo-400" title="YouTube">
                         <svg class="w-5 h-5" fill="#fff" viewBox="0 0 24 24">
                             <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
                         </svg>
@@ -69,7 +69,7 @@
                 <h3 class="text-sm font-semibold text-white mb-4">Academics</h3>
                 <ul class="space-y-2 text-sm">
                     <li><a href="/subjects" class="hover:underline">Subjects</a></li>
-                    <li><a href="/courses" class="hover:underline">Courses</a></li>
+                    <li><a href="index.php?page=docs&doc=this+is+stuarz" class="hover:underline">Academics</a></li>
                     <li><a href="/grades" class="hover:underline">Grades & Reports</a></li>
                     <li><a href="/calendar" class="hover:underline">Academic Calendar</a></li>
                 </ul>
@@ -91,7 +91,7 @@
                 <h3 class="text-sm font-semibold text-white mb-4">About Stuarz</h3>
                 <ul class="space-y-2 text-sm">
                     <li><a href="/about" class="hover:underline">About Us</a></li>
-                    <li><a href="/team" class="hover:underline">Team</a></li>
+                    <li><a href="index.php?page=company" class="hover:underline">Company</a></li>
                     <li><a href="/careers" class="hover:underline">Careers</a></li>
                     <li><a href="/blog" class="hover:underline">Blog & News</a></li>
                 </ul>
